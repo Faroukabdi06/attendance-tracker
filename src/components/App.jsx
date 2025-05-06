228c6c2f59b32612d7b121d4738f95412436e503
Farouk Abdi <abdifarouk58@gmail.com>
2025-05-06 20:19:31 +0300

    completed task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,5 @@
 import StudentList from './StudentList';
+import { useState } from 'react';
 
 // TODO 1: Add a resetAttendance function to mark all students as absent
 
@@ -9,16 +10,36 @@ function App() {
     { id: 3, name: 'Charlie', present: false },
     { id: 4, name: 'Diana', present: false }
   ];
+const [students, setStudents] = useState(initialStudents)
+// Todoo 1
+const resetAttendance = ()=>{
+  const updated = students.map(student=>({
+    ...student, present:false
+})
 
+  )
+  setStudents(updated)
+}
   // TODO 2: Create a toggleAttendance function and pass it to StudentList
-
+  const toggleAttendance  = (id) => {
+    const updated  = students.map(student=>
+      student.id === id ?
+      {...student, present : !student.present} : student
+    )
+    setStudents(updated)
+   }
+   const count = students.filter(student=>student.present).length
   return (
     <div className='container'>
       <h1>🏫 Attendance Tracker</h1>
-      <p>Present: </p>
+      <p>Present:{count} </p>
 
       {/* TODO 3: Add a Reset Attendance button here */}
-      <StudentList />
+      <button onClick={resetAttendance}>Reset</button>
+      <StudentList
+      students={students}
+      onToggle={toggleAttendance}
+      />
     </div>
   );
 }
--- src/components/StudentList.jsx
+++ src/components/StudentList.jsx
@@ -1,8 +1,19 @@
-
+import Student from "./Student";
 function StudentList({ students, onToggle }) {
+  console.log(students)
   return (
     <div>
       {/* TODO 4: Map through students and render Student components */}
+      <ul>
+        {students.map((student)=>(
+          // console.log(student)
+          <Student
+          student = {student}
+          key={student.id}
+          onToggle={onToggle}
+          />
+        ))}
+      </ul>
     </div>
   );
 }
